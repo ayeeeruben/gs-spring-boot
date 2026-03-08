85b579c0838709b371574a74bb91ba15f7ff6532
ayeeeruben <marinez.ruben210@gmail.com>
2026-03-07 20:26:51 -0800

    dockerfile added

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @GetMapping("/")
+  @GetMapping("/greeting")
   public String index() {
     return "Greetings from Spring Boot!";
+  }
+   @GetMapping("/hey")
+  public String hey() {
+    return "wasss poppinggg!";
   }
 
 }
